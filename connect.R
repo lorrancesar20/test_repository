d5b63d271e6aa5794cc4573d6e71de89972d83a3
lorrancesar20 <45302688+lorrancesar20@users.noreply.github.com>
2020-10-23 10:01:11 -0300

    testando commit

---
+++ connect.R
@@ -0,0 +1,36 @@
+
+
+install.packages("RMySQL")
+library(RMySQL)  
+
+#conectando dw
+con_dw <- dbConnect(RMySQL::MySQL(), 
+                    host = "dw.clickbus.com.br",
+                    user = "esilva", 
+                    password = "acesso022020*",
+                    db = "dw")
+
+
+#conectando storage
+con_sto <- dbConnect(RMySQL::MySQL(), 
+                     host = "dw.clickbus.com.br",
+                     user = "esilva", 
+                     password = "acesso022020*",
+                     db = "storage")
+
+
+#conectando phoenix
+con_pho <- dbConnect(RMySQL::MySQL(),
+                     host = "phoenix-db-ro.clickbus.net",
+                     user = "esilva",
+                     password = "eevahshah0Eod3M",
+                     db = "platform")
+
+#conectando dw
+con_buser <- dbConnect(RMySQL::MySQL(), 
+                       host = "dw.clickbus.com.br",
+                       user = "esilva", 
+                       password = "acesso022020*",
+                       db = "crawlers")
+
+
